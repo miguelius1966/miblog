cb9b0e4750e752a1e32d325897ecefcc3fe3b855
Miguel Angel <miguelangel.garciablazquez@gmail.com>
2020-03-16 14:08:49 +0100

    Se modifica index.php para añadir que se utilizará GitHub.

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 	echo <<<END
-Voy a tratar de montar un blog desde cero utilizando HTML5, CSS, PHP y MySQL. Lo primero que se me ocurre es pensar en el documento HTML5 que proporcionará el esqueleto del blog que utilizará lo típico de HTML5 como divisiones, secciones, menú de navegación, etc. Lo que tengo claro es que los artículos con sus títulos y su contenido los tiene que leer de la BBDD y también las categorías, los menús. 
+Voy a tratar de montar un blog desde cero utilizando HTML5, CSS, PHP y MySQL y GitHub. Lo primero que se me ocurre es pensar en el documento HTML5 que proporcionará el esqueleto del blog que utilizará lo típico de HTML5 como divisiones, secciones, menú de navegación, etc. Lo que tengo claro es que los artículos con sus títulos y su contenido los tiene que leer de la BBDD y también las categorías, los menús. 
 END;
 ?>
