f6cc9fcb15360f218861c4550bfe2effc655f884
Miguel Angel <miguelangel.garciablazquez@gmail.com>
2020-03-19 19:05:09 +0100

    Se modifica el fichero index.php

--- index.php
+++ index.php
@@ -9,49 +9,50 @@
 <link rel="stylesheet" href="miblog.css">
 </head>
 <body>
-<header>
-<!-- Comentario para probar git -->
-<h1>Este es el título principal del sitio web</h1>
-</header>
-<nav>
-  <ul>
-    <?php printMenu() ?>
-  </ul>
-</nav>
-<section>
-  <article>
+  <div id="agrupar">
     <header>
-      <hgroup>
-        <h1>Título del mensaje uno</h1>
-        <h2>Subtítulo del mensaje uno</h2>
-      </hgroup>
-      <p>publicado 10-12-2011</p>
+    <h1>Este es el título principal del sitio web</h1>
     </header>
-    Este es el texto de mi primer mensaje
-    <figure><img src=”miguel-tres-gatitos.jpg”>
-      <figcaption>Esta es la imagen del primer mensaje</figcaption>
-    </figure>
-    <footer>
-      <p>comentarios (0)</p>
-    </footer>
-  </article>
-  <article>
-    <header>
-      <hgroup>
-        <h1>Título del mensaje dos</h1>
-        <h2>Subtítulo del mensaje dos</h2>
-      </hgroup>
-      <p>publicado 15-12-2011</p>
-    </header>Este es el texto de mi segundo mensaje
-    <footer>
-      <p>comentarios (0)</p>
-    </footer>
-  </article>
-</section>
-<aside>
-  <blockquote>Mensaje número uno</blockquote>
-  <blockquote>Mensaje número dos</blockquote>
-</aside>
-<footer>Derechos Reservados &copy; 2019</footer>
+    <nav id="menu">
+      <ul>
+        <?php printMenu() ?>
+      </ul>
+    </nav>
+    <section id="seccion">
+      <article>
+        <header>
+          <hgroup>
+            <h1>Título del mensaje uno</h1>
+            <h2>Subtítulo del mensaje uno</h2>
+          </hgroup>
+          <p>publicado 10-12-2011</p>
+        </header>
+        Este es el texto de mi primer mensaje
+        <figure><img src=”miguel-tres-gatitos.jpg”>
+          <figcaption>Esta es la imagen del primer mensaje</figcaption>
+        </figure>
+        <footer>
+          <p>comentarios (0)</p>
+        </footer>
+      </article>
+      <article>
+        <header>
+          <hgroup>
+            <h1>Título del mensaje dos</h1>
+            <h2>Subtítulo del mensaje dos</h2>
+          </hgroup>
+          <p>publicado 15-12-2011</p>
+        </header>Este es el texto de mi segundo mensaje
+        <footer>
+          <p>comentarios (0)</p>
+        </footer>
+      </article>
+    </section>
+    <aside id="columna">
+      <blockquote>Mensaje número uno</blockquote>
+      <blockquote>Mensaje número dos</blockquote>
+    </aside>
+    <footer id="pie">Derechos Reservados &copy; 2020</footer>
+  </div>
 </body>
 </html>
