e1b28941317f6c81699b61c4cb14b54f46102e32
Miguel Angel <miguelangel.garciablazquez@gmail.com>
2020-03-20 11:00:06 +0100

    Se modifica index.php para cambiar la cabecera del documento HTML

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
 <html lang="es">
 <head>
 <meta charset="utf-8">
-<meta name="description" content="Ejemplo de HTML5">
-<meta name="keywords" content="HTML5, CSS3, JavaScript">
-<title>Este texto es el título del documento</title>
+<meta name="description" content="Blog utilizando HTML5">
+<meta name="keywords" content="HTML5, CSS3, JavaScript, MySQL">
+<title>Blog de miguelius</title>
 <link rel="stylesheet" href="miblog.css">
 </head>
 <body>
